45aac119ec13689448a07dd2f6efd728d8587fe3
Alec <aroarks1@gmail.com>
2023-03-03 22:59:32 -0500

    Add basic coloring and grid resizing

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const createGrid = function(gridSize) {
+    resetGrid();
     const gridContainer = document.querySelector('div.gridContainer');
     for (let i = 0; i < gridSize; i++) {
         const newGridRow = document.createElement('div');
@@ -10,4 +11,21 @@ const createGrid = function(gridSize) {
             newGridRow.append(newGridCell);
         }
     }
+    activateCellColoring();
+};
+
+const resetGrid = function() {
+    const itemsToRemove = document.querySelectorAll('div.gridRow');
+    itemsToRemove.forEach((item) => {
+        item.remove();
+    });
+};
+
+const activateCellColoring = function() {
+    const colorCellList = document.querySelectorAll('div.gridCell');
+    colorCellList.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.setAttribute('style', 'background: black');
+        });
+    });
 };
